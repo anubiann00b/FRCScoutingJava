f77f2402232ddaa2c676e514359d733506b416b1
Shreyas <skraman1999@gmail.com>
2015-03-01 11:40:43 -0800

    Get the JSON.

--- src/com/saintsrobotics/firebase/FRCScouting.java
+++ src/com/saintsrobotics/firebase/FRCScouting.java
@@ -1,27 +1,53 @@
 package com.saintsrobotics.firebase;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.Firebase.AuthResultHandler;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import org.json.JSONObject;
 
 public class FRCScouting {
 
     public static void main(String[] args) throws InterruptedException {
-        Firebase myFirebaseRef = new Firebase("https://frc-scouting.firebaseio.com/");
-
-        myFirebaseRef.child("message").setValue("ayyyyyyy");
-        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() {
+        Firebase root = new Firebase("https://frc-scouting.firebaseio.com/");
+        
+        root.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                System.out.println(snapshot.getValue());
-                System.exit(0);
+            public void onDataChange(DataSnapshot ds) {
+                Log.d("Data recieved.");
+                JSONObject obj = new JSONObject(ds.toString().substring(13).replace('=', ':')).getJSONObject("value");
+                Log.d(obj.toString(2));
             }
 
             @Override
-            public void onCancelled(FirebaseError error) {
+            public void onCancelled(FirebaseError fe) {
+                Log.e(fe.toString());
             }
         });
-        Thread.sleep(10000);
+        root.authAnonymously(new AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                Log.d(authData.toString());
+            }
+
+            @Override
+            public void onAuthenticationError(FirebaseError error) {
+                Log.e(error.toString());
+            }
+        });
+        while(true) Thread.sleep(5000);
+    }
+}
+
+class Log {
+    
+    public static void d(String str) {
+        System.out.println(str);
+    }
+    
+    public static void e(String str) {
+        System.out.println(str);
     }
 }
